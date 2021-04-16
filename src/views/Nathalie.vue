ddf7cbb3b19b5c13daf4b56857c9a17058183c9e
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-04-16 15:30:43 +0200

    Added links

--- src/views/Nathalie.vue
+++ src/views/Nathalie.vue
@@ -3,14 +3,18 @@
       <div class="content">
           <h1 class="grid-item-header">Profile</h1>
           <div class="grid-item-container">
-          <img id="grid-item-image1" src="../assets/Nathalie.jpg" alt="Profile picture Nathalie" height="300">
+              <a id="grid-item-image1" href="#">
+                <img src="../assets/Nathalie.jpg" alt="Profile picture Nathalie" height="300">
+              </a>
           <p class="grid-item-profile">Hi there! My name is Nathalie Lindqvist, a 26 year old woman who has a passion for that little extra. May it be a song performed by a favourite band such as Kent or Avenged Sevenfold, an exquisite scoop of chocolate ice cream or an extra hour spent on making a freshly coded web site look just right. Frontend development is songthing that will never be "just a job" for me. When there's so much to do and learn it's impossible to be bored!</p>
           </div>
           <hr>
 
           <h1 class="grid-item-header">Expertise</h1>
           <div class="grid-item-container">
-          <img id="grid-item-image2" src="../assets/expertise.png" alt="Simple HTML tag symbole" height="300">
+            <a id="grid-item-image2" href="#">
+              <img src="../assets/expertise.png" alt="Simple HTML tag symbole" height="300">
+            </a>
           <div class="grid-item-expertise">
             <p>Through studies I have acquired theoretical and/or practical knowledge in:</p>
             <ul>
@@ -30,17 +34,19 @@
 
           <h1 class="grid-item-header">Portfolio</h1>
           <div class="grid-item-container">
-          <a href="http://mobelbiblioteket.se/" target="_blank">
-            <img id="grid-item-image3" src="../assets/chair.jpg" alt="Arm chair" height="300">
+          <a id="grid-item-image3" href="http://mobelbiblioteket.se/" target="_blank">
+            <img src="../assets/chair.jpg" alt="Arm chair" height="300">
           </a>
           <section class="grid-item-portfolio1">
             <h3>Möbelbiblioteket (real project)</h3>
-            <p>During spring and summer 2019 I had the honor to work as a junior frontend developer on the project Möbelbiblioteket, a service where users can borrow furniture from one another. You can check it out by <a id="text-link" href="http://mobelbiblioteket.se/" target="_blank">clicking here!</a></p>
+            <p>During spring and summer 2019 I had the honor to work as a junior frontend developer on the project Möbelbiblioteket, a service where users can borrow furniture from one another. You can check it out by <a class="text-link" href="http://mobelbiblioteket.se/" target="_blank">clicking here!</a></p>
           </section>
-              <img id="grid-item-image4" src="../assets/doctor.jpg" alt="Cild playing doctor" height="300">
+            <a id="grid-item-image4" href="https://doktor.se/" target="_blank">
+              <img src="../assets/doctor.jpg" alt="Child playing doctor" height="300">
+              </a>
           <section class="grid-item-portfolio2">
               <h3>Doktor.se (fictional project)</h3>
-              <p>As part of a project group in 2018 i got to take part in shaping the app Doktor.se, an app where patients can get help from healthcare staff through video calls, ask for advice or renew existing prescriptions.</p>
+              <p>As part of a project group in 2018 i got to take part in shaping the app Doktor.se, an app where patients can get help from healthcare staff through video calls, ask for advice or renew existing prescriptions. <a class="text-link" href="https://doktor.se/" target="_blank">Click here</a> to go to their web page.</p>
           </section>
           </div>
 
@@ -85,12 +91,14 @@ export default {
     filter: opacity(90%);
     transition: transform 0.5s;
     clip-path: polygon(0 0%, 100% 0%, 100% 94%, 0% 100%);
+    height: 300px;
 }
 
 #grid-item-image2 {
     grid-column: 1/2;
     grid-row: 1/2;
     transition: transform 0.5s;
+    height: 300px;
 }
 
 #grid-item-image3 {
@@ -99,6 +107,7 @@ export default {
     filter: opacity(90%);
     transition: transform 0.5s;
     clip-path: polygon(0 10%, 100% 0%, 100% 100%, 0% 94%);
+    height: 300px;
 }
 
 #grid-item-image4 {
@@ -107,6 +116,7 @@ export default {
     filter: opacity(90%);
     transition: transform 0.5s;
     clip-path: polygon(0 0%, 100% 5%, 100% 100%, 0% 100%);
+    height: 300px;
 }
 
 #grid-item-image1:hover,
@@ -137,11 +147,11 @@ export default {
     grid-row: 4/6;
 }
 
-#text-link {
+.text-link {
     text-decoration: none;
 }
 
-#text-link:hover {
+.text-link:hover {
     text-decoration: underline;
     color: #31af7f;
 }
@@ -153,6 +163,7 @@ export default {
 ul {
     max-width: 120px;
     margin: auto;
+    list-style-type: square;
 }
 
 </style>
