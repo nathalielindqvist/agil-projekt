2e4410c681cc018a3d6d333b3e976db462015ef7
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-04-16 11:39:45 +0200

    Added images and basic text

---
+++ src/views/Nathalie.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+      <div class="content">
+          <h1>Profile</h1>
+          <img src="../assets/Nathalie.jpg" alt="Profile picture Nathalie" height="250">
+          <p>Hi there! My name is Nathalie Lindqvist, a 26 year old woman who has a passion for that little extra. May it be a song performed by a favourite band such as Kent or Avenged Sevenfold, an exquisite scoop of chocolate ice cream or an extra hour spent on making a freshly coded web site look just right. Frontend development is songthing that will never be "just a job" for me. When there's so much to do and learn it's impossible to be bored!</p>
+          <hr>
+          <h1>Expertise</h1>
+          <img src="../assets/expertise.png" alt="Simple HTML tag symbole" height="250">
+          <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>Node</li>
+              <li>MySQL</li>
+              <li>Vue</li>
+              <li>React</li>
+          </ul>
+          <hr>
+          <h1>Portfolio</h1>
+          <section>
+          <img src="../assets/chair.jpg" alt="Arm chair" height="250">
+          <h3>Möbelbiblioteket (real project)</h3>
+          <p>During spring and summer 2019 I had the honor to work as a junior frontend developer on the project Möbelbiblioteket, a service where users can borrow furniture from one another. You can check it out by <a href="http://mobelbiblioteket.se/" target="_blank">clicking here!</a></p>
+          </section>
+          <section>
+              <img src="../assets/doctor.jpg" alt="Cild playing doctor" height="250">
+              <h3>Doktor.se (fictional project)</h3>
+            <p>As part of a project group in 2018 i got to take part in shaping the app Doktor.se, an app where patients can get help from healthcare staff through video calls, ask for advice or renew existing prescriptions.</p>
+          </section>
+
+
+      </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
