180fd0bca2fcc80cc44e6aa9502f4044ebb47c59
pakorn <pakorn_00@hotmail.com>
2021-04-20 13:42:22 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,9 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-<<<<<<< HEAD
-    <router-link to="/pakorn">Pakorn</router-link>
-=======
     <router-link to="/pakorn">Pakorn</router-link> |
     <router-link to="/danny">Dan</router-link>
->>>>>>> master
+
   </div>
   <div id="mainContainer">
     <!-- <router-link to="/Axel">Axel</router-link> -->
